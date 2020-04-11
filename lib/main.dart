78b0d05385f1b0a6b67c45ed9030c39cdbd7a7cc
jccard <j.c.card@gmail.com>
2020-04-11 14:57:41 -0700

    S06.49 - Adding Material Icons

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,60 @@ void main() {
                   color: Colors.white,
                 ),
               ),
+              Divider(
+                indent: 50.0,
+                endIndent: 50.0,
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(
+                  10.0,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.add_shopping_cart,
+                      size: 30.0,
+                      color: Colors.teal[200],
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'Put it in the cart',
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                padding: EdgeInsets.all(
+                  10.0,
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.note_add,
+                      size: 30.0,
+                      color: Colors.teal[200],
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'Write it down',
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
