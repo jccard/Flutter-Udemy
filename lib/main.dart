30013bc1a63f8f130cab7cd19adab0baf7c88a6c
jccard <j.c.card@gmail.com>
2020-04-11 12:13:06 -0700

    Code Challenge - layout with columns and rows

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,51 @@ void main() {
     MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Row(
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      width: 100.0,
+                      color: Colors.red,
+                      child: Text('red column'),
+                    ),
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.yellow,
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      width: 100.0,
+                      color: Colors.blue,
+                      child: Text('blue column'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     ),
   );
