44b91707789d84de45143b649ef3695bf67e2df0
jccard <j.c.card@gmail.com>
2020-04-18 11:59:41 -0700

    finished magic 8 ball - adding Colorz was the only customization

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:colorz/colorz.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colorz.abeBlue,
+        appBar: AppBar(
+          backgroundColor: Colorz.aaronBurr,
+          title: Text(
+            'Magic 8 Ball',
+            style: TextStyle(
+              color: Colorz.abyssalFissure,
+            ),
+          ),
+        ),
+        body: BallPage(),
+      ),
+    ),
+  );
+}
+
+// Ball Page Stateful Widget
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballImage = 1;
+
+  void shake8ball() {
+    ballImage = Random().nextInt(5) +1;
+    print('The magic 8 ball has been shaken... Result is: $ballImage');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  shake8ball();
+                });
+              },
+              child: Image.asset('images/ball$ballImage.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+// END Ball Page Stateful Widget
